1921f1756555d3c865b00abdc7620263b9eddabf
fydar <fydar.dev@gmail.com>
2021-09-16 23:03:00 +0100

    ✨ Added a console project for parsing and testing the passwords.csv file

---
+++ src/PasswordChecker/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using TextConstraints;
+using TextConstraints.Tree;
+
+namespace PasswordChecker
+{
+	internal class Program
+	{
+		private static async Task Main(string[] args)
+		{
+			Console.ForegroundColor = ConsoleColor.DarkGray;
+			Console.WriteLine("");
+			Console.WriteLine(" ╔══ [PasswordChecker] ═════════════════════════════════════════════╗");
+			Console.WriteLine(" ║ A console application that outputs bad passwords.                ║");
+			Console.WriteLine(" ╚══════════════════════════════════════════════════════════════════╝");
+			Console.WriteLine("");
+			Console.ResetColor();
+
+
+			var tree = WordTree.ReadFromBytes(File.ReadAllBytes("output.bin"));
+
+			var policy = new TextPolicyBuilder("password_policy")
+				.UseTextRule(new NotSingleWordTextRule(tree))
+				.Build();
+
+			var passwordsFile = new FileInfo("passwords.csv");
+			using var reader = passwordsFile.OpenText();
+
+			// Skip the first line...
+			await reader.ReadLineAsync();
+
+			while (!reader.EndOfStream)
+			{
+				string line = await reader.ReadLineAsync();
+
+				var elements = line.Split(',');
+				if (elements.Length < 4)
+				{
+					continue;
+				}
+
+				var passwordElement = elements[2].Trim();
+
+				var result = policy.Evaluate(passwordElement);
+
+				if (result.Status == RuleStatus.Failed)
+				{
+					Console.WriteLine($"The {elements[3]} user '{elements[0]}' password of '{passwordElement}' failed to pass the password policy.");
+				}
+			}
+		}
+	}
+}
