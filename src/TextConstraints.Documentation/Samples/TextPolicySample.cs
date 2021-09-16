8bbd07588ce0d00e8484e190ea0a320a6b9e8254
fydar <fydar.dev@gmail.com>
2021-09-16 23:31:59 +0100

    ✨ Added a password requirement for characters of a certain category

--- src/TextConstraints.Documentation/Samples/TextPolicySample.cs
+++ src/TextConstraints.Documentation/Samples/TextPolicySample.cs
@@ -7,6 +7,19 @@ namespace RPGCore.Documentation.Samples
 {
 	public class TextPolicySample
 	{
+		public static void SimplePolicy()
+		{
+			#region simple_policy
+			var tree = WordTree.ReadFromBytes(File.ReadAllBytes("output.bin"));
+
+			var policy = new TextPolicyBuilder("password_policy")
+				.UseTextRule(new RequireCharactersTextRule(1, char.IsUpper))
+				.UseTextRule(new RequireCharactersTextRule(1, char.IsNumber))
+				.UseTextRule(new NotSingleWordTextRule(tree))
+				.Build();
+			#endregion simple_policy
+		}
+
 		public static void Run()
 		{
 			#region policy
@@ -17,6 +30,10 @@ namespace RPGCore.Documentation.Samples
 				.UseTextRule(new LengthTextRule(4, 32))
 				.UseTextRule(new CharsetTextRule("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"))
 				.UseTextRule(new NotSingleWordTextRule(tree))
+				.UseTextRule(new RequireCharactersTextRule(1, char.IsUpper))
+				.UseTextRule(new RequireCharactersTextRule(1, char.IsNumber))
+				.UseTextRule(new RequireCharactersTextRule(1, c => char.IsPunctuation(c) || char.IsSymbol(c)))
+				.UseTextRule(new RequireCharactersTextRule(1, char.IsLower))
 				.Build();
 			#endregion create_policy
 
---
+++ src/TextConstraints/RequireCharactersTextRule.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace TextConstraints
+{
+	/// <summary>
+	/// Ensures the text has an amount of characters that matcha predicate.
+	/// </summary>
+	public class RequireCharactersTextRule : ITextRule
+	{
+		/// <summary>
+		/// The amount of characters that must match the predicate.
+		/// </summary>
+		public int MinAmount { get; }
+
+		/// <summary>
+		/// A predicate used to determine whether the character meets the condition of this rule.
+		/// </summary>
+		public Func<char, bool> Predicate { get; }
+
+		/// <summary>
+		/// Constructs a new instance of the <see cref="RequireCharactersTextRule"/> class.
+		/// </summary>
+		/// <param name="minAmount">The amount of characters that must match the predicate.</param>
+		/// <param name="predicate">A predicate used to determine whether the character meets the condition of this rule.</param>
+		public RequireCharactersTextRule(int minAmount, Func<char, bool> predicate)
+		{
+			MinAmount = minAmount;
+			Predicate = predicate;
+		}
+
+		/// <inheritdoc/>
+		public TextRuleResult Evaluate(ReadOnlySpan<char> text)
+		{
+			int matches = 0;
+
+			foreach (var character in text)
+			{
+				if (Predicate.Invoke(character))
+				{
+					matches++;
+				}
+			}
+
+			if (matches < MinAmount)
+			{
+				return TextRuleResult.Failed();
+			}
+
+			return TextRuleResult.Passed();
+		}
+	}
+}
